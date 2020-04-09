c46b2831522179e3e14463c7a9c9c20a6f021245
carl <carl.hume@gmail.com>
2020-04-09 19:41:07 -0400

    Perlin Noise Map Generation

---
+++ Assets/Scripts/CameraDrag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDrag : MonoBehaviour
+{
+    public float dragSpeed = 1.1f;
+    private Vector3 dragOrigin;
+ 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+ 
+        if (!Input.GetMouseButton(0)) return;
+ 
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(-pos.x * dragSpeed, -pos.y * dragSpeed, 0);
+
+        // TODO: >> cnh >> Prevent camera from scrolling too far past the edges of the map
+        transform.Translate(move, Space.World);  
+        
+    }
+}
---
+++ Assets/Scripts/RandomHexTileGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomHexTileGenerator : MonoBehaviour
+{
+    public GameObject WaterHexTilePrefab;
+    public GameObject ForestHexTilePrefab;
+    public GameObject PlainsHexTilePrefab;
+    public GameObject DesertHexTilePrefab;
+    public GameObject MountainHexTilePrefab;
+
+    private GameObject [] prefabs;
+
+    public int mapWidthInHexes;
+    public int mapHeightInHexes;
+
+    public float tileXOffset;
+    public float tileYoffset;
+
+    private System.Random random = new System.Random();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadPrefabs();
+        createHexTilemap();
+    }
+
+    private void loadPrefabs() {
+        prefabs = new GameObject[5];
+        prefabs[0] = WaterHexTilePrefab;
+        prefabs[1] = ForestHexTilePrefab;
+        prefabs[2] = PlainsHexTilePrefab;
+        prefabs[3] = DesertHexTilePrefab;
+        prefabs[4] = MountainHexTilePrefab;
+    }
+
+    private void createHexTilemap() {
+        //Game object which is the parent of all the hex tiles
+        GameObject hexGridGO = new GameObject( "HexGrid" );
+
+        for( int x = 0; x < mapWidthInHexes; x++ ) {
+            for( int y = 0; y < mapHeightInHexes; y++ ) {
+                GameObject hexToPlaceGO = chooseHex();
+
+                if( isEvenRow( y ) ) {
+                    hexToPlaceGO.transform.position = new Vector3( x * tileXOffset + tileXOffset / 2, y * tileYoffset, 0.01f );
+                } else {
+                    hexToPlaceGO.transform.position = new Vector3( x * tileXOffset, y * tileYoffset, 0.01f );
+                }
+            }
+        }
+
+    }
+
+    private GameObject chooseHex() {
+        GameObject prefab = prefabs[ random.Next( 0, prefabs.Length ) ];
+        return Instantiate( prefab );
+    }
+
+    private bool isEvenRow( int row ) {
+        return row % 2 == 0;
+    }
+
+}
